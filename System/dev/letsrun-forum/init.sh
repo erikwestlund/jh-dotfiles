9f4dcf785d9b637b64dc2b8eaf017cc959ecd056
Erik Westlund <edbwestlund@gmail.com>
2018-12-13 10:20:55 -0600

    fixes

--- System/dev/letsrun-forum/init.sh
+++ System/dev/letsrun-forum/init.sh
@@ -4,10 +4,11 @@
 
 # get code
 rm -rf $HOME/code/letsrun-forum
-git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/docker-letsrun-forum letsrun-wp
+cd $HOME/code
+git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/docker-letsrun-forum letsrun-forum
 cd $HOME/code/letsrun-forum
 
 ./develop sync
-./develop up
+./develop up -d
 ./develop db_seed
 ./develop down
--- System/dev/letsrun-wp/init.sh
+++ System/dev/letsrun-wp/init.sh
@@ -4,10 +4,11 @@
 
 # get code
 rm -rf $HOME/code/letsrun-wp
+cd $HOME/code
 git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/docker-letsrun-wp letsrun-wp
 cd $HOME/code/letsrun-wp
 
 ./develop sync
-./develop up
+./develop up -d
 ./develop db_seed
 ./develop down
