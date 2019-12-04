3f6c9c7f5f54f92c7f7fd53e4f6b8cdfebaf948f
Erik Westlund <edbwestlund@gmail.com>
2019-12-04 11:03:17 -0600

    fix up nfs

--- System/dev/docker-parallels.sh
+++ System/dev/docker-parallels.sh
@@ -6,4 +6,12 @@ https://github.com/docker/toolbox
 # Install Parallels Desktop Pro, license
 brew install docker-machine-parallels
 
+# Install NFS for speed
+brew install docker-machine-nfs
+
+# Run this mount
+docker-machine-nfs Docker \
+    --mount-opts="noacl,async,nolock,vers=3,udp,noatime,actimeo=2" \
+    --shared-folder="/Users/erik/code"
+
 docker-machine create --driver parallels --parallels-disk-size "51200" --parallels-memory 4096 --parallels-cpu-count 2 Docker
--- System/dev/docker-vbox.sh
+++ System/dev/docker-vbox.sh
@@ -9,12 +9,12 @@ https://github.com/docker/toolbox
 # Install NFS for speed
 brew install docker-machine-nfs
 
+# Set up
+docker-machine create --driver virtualbox --virtualbox-no-vtx-check --virtualbox-disk-size "51200" --virtualbox-memory 4096 --virtualbox-cpu-count 2  --virtualbox-hostonly-cidr "10.10.10.1/24" Docker
+
 # Run this mount
 docker-machine-nfs Docker \
     --mount-opts="noacl,async,nolock,vers=3,udp,noatime,actimeo=2" \
     --shared-folder="/Users/erik/code"
-
-# Set up
-docker-machine create --driver virtualbox --virtualbox-no-vtx-check --virtualbox-disk-size "51200" --virtualbox-memory 4096 --virtualbox-cpu-count 2  --virtualbox-hostonly-cidr "10.10.10.1/24" Docker
 
 # Set up virtualbox image.  Do networking, host mode
