3556d53fe876d310cf108d3f5959e508fb17eb6d
Erik Westlund <edbwestlund@gmail.com>
2019-12-04 10:59:58 -0600

    docker-nfs

--- System/dev/docker-vbox.sh
+++ System/dev/docker-vbox.sh
@@ -6,6 +6,14 @@ brew cask install virtualbox
 # Download docker-toolbox
 https://github.com/docker/toolbox
 
+# Install NFS for speed
+brew install docker-machine-nfs
+
+# Run this mount
+docker-machine-nfs Docker \
+    --mount-opts="noacl,async,nolock,vers=3,udp,noatime,actimeo=2" \
+    --shared-folder="/Users/erik/code"
+
 # Set up
 docker-machine create --driver virtualbox --virtualbox-no-vtx-check --virtualbox-disk-size "51200" --virtualbox-memory 4096 --virtualbox-cpu-count 2  --virtualbox-hostonly-cidr "10.10.10.1/24" Docker
 
