08b8eb840446a8083624b79d977dcba5d62a44f0
Erik Westlund <edbwestlund@gmail.com>
2019-12-05 16:17:32 -0600

    little mysql fixes

--- System/dev/services-vm/mysql/install.sh
+++ System/dev/services-vm/mysql/install.sh
@@ -4,12 +4,12 @@ wget https://repo.percona.com/apt/percona-release_0.1-6.$(lsb_release -sc)_all.d
 dpkg -i percona-release_0.1-6.$(lsb_release -sc)_all.deb
 apt update
 
+apt -y install  percona-server-server-5.7 percona-server-client-5.7 percona-toolkit
 
 #copy mysqld.cnf from services-vm/mysql to /etc/mysql/conf.d/mysqld.cnf
 # run:
 service mysql restart
 
-apt -y install  percona-server-server-5.7 percona-server-client-5.7 percona-toolkit
 mysql -u root -psecret -e "create database letsrun;"
 mysql -u root -psecret -e "create database letsrun_com;"
 mysql -u root -psecret -e "create database wordpress;"
