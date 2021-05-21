ba0a21f69ea156a1965c07bd761473be789626ad
Erik Westlund <erikwestlund@hey.com>
2021-05-21 13:26:02 -0500

    new aliases

--- System/dev.sh
+++ System/dev.sh
@@ -27,7 +27,8 @@ brew install gifsicle
 
 # install node & npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-nvm install v13
+nvm install v15
+# nvm install v13 -- install this using a rosetta console
 
 # valet
 composer global require laravel/valet
